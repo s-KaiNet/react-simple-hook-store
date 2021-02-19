cb1d167c4fed02bfe1a938818a3b53e089338e8b
Sergei Sergeev <sergeev.srg@gmail.com>
2021-02-20 01:22:27 +0300

    Removed dist folder.

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { MethodsMap, StoreActions, UseStoreReturn, IStore } from "./types";
-export declare const createStore: <S, A extends MethodsMap<A>>(initialState: S, actions: StoreActions<S, A>) => {
-    useStore: UseStoreReturn<S, A>;
-    batchUpdates: (callback: () => void) => void;
-    store: IStore<S, A>;
-};
-export * from "./types";
